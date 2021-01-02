d39e2f77ff7aa256360406365b3b9a012db57150
Angela Mitchell <anglarenmit@gmail.com>
2021-01-01 18:06:44 -0600

    Displayed form values in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component, ViewChild } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild("f") userForm: NgForm;
-
-  subscriptions: string[] = ["Basic", "Advanced", "Pro"];
+  @ViewChild('f') signupForm: NgForm;
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  user = {
+    email: '',
+    subscription: '',
+    password: '',
+  };
+  submitted: boolean = false;
 
   onSubmit() {
-    console.log(this.userForm.value);
+    this.submitted = true;
+
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.subscription = this.signupForm.value.userData.subscription;
+    this.user.password = this.signupForm.value.userData.password;
+
+    this.signupForm.reset();
   }
 }
